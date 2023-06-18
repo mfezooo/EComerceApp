145e434ea11f8d7f86ef2a4cd1fe9966e29752ac
Eng Mohamed Fayez <mfayez@Delta.Grand>
2023-06-18 13:17:59 +0300

    addMouseHove

--- src/app/Directives/light-box.directive.ts
+++ src/app/Directives/light-box.directive.ts
@@ -1,13 +1,18 @@
-import { Directive, ElementRef } from '@angular/core';
+import { Directive, ElementRef, HostListener } from '@angular/core';
 
 @Directive({
   selector: '[LightBox]'
 })
 export class LightBoxDirective {
 
-  constructor(elementRef : ElementRef)
+  constructor(private elementRef : ElementRef)
    { 
     elementRef.nativeElement.style.border="2px solid darkblue";
   }
-
+@HostListener('mouseover') onMouseOver(){
+ this.elementRef.nativeElement.style.border="2px solid yellow";
+}
+@HostListener('mouseout') onMouseOut(){
+  this.elementRef.nativeElement.style.border="2px solid darkblue";
+ }
 }
